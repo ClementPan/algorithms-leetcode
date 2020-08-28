ea9987d59b987a8024570d6a0fb973f23197c50c
ClementPan <zzz2999582@gmail.com>
2020-08-28 19:27:23 +0800

    num14: edit

--- num14_longestCommonPrefix.js
+++ num14_longestCommonPrefix.js
@@ -7,11 +7,10 @@ const longestCommonPrefix = function (strs) {
     return ''
   } else {
     const strings = strs
-    let output = ''
     let validPrefix = ''
 
     for (let i = 0; i < strings[0].length; i++) {
-      inspecting = strings[0][i]
+      let inspecting = strings[0][i]
       for (let j = 0; j < strings.length; j++) {
         if (strings[j][i] !== inspecting) {
           return validPrefix
